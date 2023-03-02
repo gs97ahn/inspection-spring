2e3628d8839252a1ed90c5884f82fa50dfb420ce
gs97ahn <gs97ahn@naver.com>
2023-03-02 14:20:12 +0900

    [INIT] first commit

---
+++ src/main/java/nogamsung/inspectionspring/InspectionSpringApplication.java
@@ -0,0 +1,13 @@
+package nogamsung.inspectionspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspectionSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nogamsung/inspectionspring/InspectionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package nogamsung.inspectionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspectionSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
